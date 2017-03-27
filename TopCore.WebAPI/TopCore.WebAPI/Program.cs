267a0ec9b9a54fc63020f1797d48110549420003
Top Nguyen <topnguyen92@gmail.com>
2017-03-28 01:26:35 +0700

    Initial

---
+++ TopCore.WebAPI/TopCore.WebAPI/Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace TopCore.WebAPI
+{
+    public class Program
+    {
+        private static string LaunchUrl { get; set; }
+
+        public static void Main(string[] args)
+        {
+            IWebHost host = BuildHost();
+
+            // [Important] Run Browser before Host Run
+            StartBrowser();
+
+            // Let Dance
+            host.Run();
+        }
+
+        private static IWebHost BuildHost()
+        {
+            IWebHostBuilder hostBuilder =
+                new WebHostBuilder()
+                    .UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseIISIntegration()
+                    .UseStartup<Startup>();
+
+            // Fetch Launch URL by Environment
+            FetchLaunchUrlByEnvironment();
+
+            // Set URL Listener
+            hostBuilder.UseUrls(LaunchUrl);
+            IWebHost host = hostBuilder.Build();
+
+            return host;
+        }
+
+        private static void FetchLaunchUrlByEnvironment()
+        {
+            string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var launchSettings = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Properties"))
+                .AddJsonFile("launchSettings.json", optional: true)
+                .Build();
+            LaunchUrl = launchSettings.GetSection($"profiles:{environmentName}:launchUrl").Value;
+        }
+
+        /// <summary>
+        ///     Start Browser by CMD with separate thread 
+        /// </summary>
+        private static void StartBrowser()
+        {
+            new Thread(() =>
+            {
+                try
+                {
+                    OpenBrowser();
+                }
+                catch
+                {
+                    Debug.WriteLine($"{nameof(StartBrowser)} catch!");
+                }
+            }).Start();
+        }
+
+        /// <summary>
+        ///     Open browser depend on Platform 
+        /// </summary>
+        private static void OpenBrowser()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {LaunchUrl}"));
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", LaunchUrl);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", LaunchUrl);
+            }
+            Debug.WriteLine($"{nameof(OpenBrowser)} not cover!");
+        }
+    }
+}
---
+++ TopCore.WebAPI/TopCore.WebAPI/Startup.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TopCore.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        #region ConfigureServices
+
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to add services to the container. 
+        /// </summary>
+        /// <param name="services"> The services. </param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        #endregion ConfigureServices
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            SetupLogFactory(loggerFactory);
+            SetupExceptionPage(app, env);
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        #region Configure
+
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to configure the HTTP request pipeline. 
+        /// </summary>
+        /// <param name="app">           The application. </param>
+        /// <param name="env">           The environment. </param>
+        /// <param name="loggerFactory"> The logger factory. </param>
+        private void SetupLogFactory(ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+        }
+
+        private static void SetupExceptionPage(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+        }
+
+        #endregion Configure
+    }
+}

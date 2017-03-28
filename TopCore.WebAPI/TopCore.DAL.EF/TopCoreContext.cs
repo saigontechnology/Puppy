dedcabb37416564a01cb4695a96542b2f81c33ee
Top Nguyen <topnguyen92@gmail.com>
2017-03-28 21:02:41 +0700

    temp

--- TopCore.WebAPI/TopCore.DAL.EF/TopCoreContext.cs
+++ TopCore.WebAPI/TopCore.DAL.EF/TopCoreContext.cs
@@ -1,23 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Diagnostics;
 using TopCore.DAL.Entity;
 
-namespace TopCore.DAL
+namespace TopCore.DAL.EF
 {
     public class TopCoreContext : DbContext
     {
         public TopCoreContext(DbContextOptions<TopCoreContext> options) : base(options)
         {
+            Debug.WriteLine($"{nameof(TopCoreContext)} is Created", nameof(TopCoreContext));
         }
 
         public DbSet<UserEntity> UserEntities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            Debug.WriteLine($"{nameof(TopCoreContext)} is Created", nameof(OnModelCreating));
+
             // Convention Table Name is Entity Name without Entity Postfix
             foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
                 entity.Relational().TableName = entity.DisplayName().Replace("Entity", string.Empty);
+            }
             base.OnModelCreating(modelBuilder);
+
+            Database.Migrate();
         }
     }
 }

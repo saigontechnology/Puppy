caaa00ccf15cc8bd6ebb0e8bc24f77181f9269f4
Top Nguyen <topnguyen92@gmail.com>
2017-04-04 21:09:09 +0700

    Update login model

--- TopCore.SSO/TopCore.SSO/Controllers/UserController.cs
+++ TopCore.SSO/TopCore.SSO/Controllers/UserController.cs
@@ -1,4 +1,10 @@
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TopCore.SSO.Identity;
+using TopCore.SSO.ViewModels;
 
 namespace TopCore.SSO.Controllers
 {
@@ -6,14 +12,48 @@ namespace TopCore.SSO.Controllers
     [Produces("application/json")]
     public class UserController : Controller
     {
-        /// <summary>
-        ///     GET: api/User 
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public IActionResult Get()
+        private readonly IIdentityServerInteractionService _interaction;
+        private readonly UserManager<TopCoreIdentityUser> _userManager;
+
+        public UserController(UserManager<TopCoreIdentityUser> userManager,
+            IIdentityServerInteractionService interaction)
         {
-            return Ok(new[] { "value1", "value2" });
+            _userManager = userManager;
+            _interaction = interaction;
+        }
+
+        /// <summary>
+        ///     Login 
+        /// </summary>
+        /// <param name="loginViewModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityUser = await _userManager.FindByNameAsync(loginViewModel.UserName).ConfigureAwait(true);
+
+                if (identityUser != null && await _userManager.CheckPasswordAsync(identityUser, loginViewModel.Password).ConfigureAwait(true))
+                {
+                    AuthenticationProperties properties = null;
+                    if (loginViewModel.RememberMe)
+                    {
+                        properties = new AuthenticationProperties
+                        {
+                            IsPersistent = true
+                        };
+                    }
+
+                    await HttpContext.Authentication.SignInAsync(identityUser.Id, identityUser.UserName).ConfigureAwait(true);
+
+                    return Ok(properties);
+                }
+
+                return BadRequest("Invalid username or password.");
+            }
+
+            return BadRequest(ModelState);
         }
     }
 }

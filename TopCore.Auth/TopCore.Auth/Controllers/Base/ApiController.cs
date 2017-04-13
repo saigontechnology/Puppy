92e9354a05237453ed0a0e26eab59c0902241f98
Top Nguyen <topnguyen92@gmail.com>
2017-04-13 22:47:59 +0700

    Filter work like a charm

--- TopCore.Auth/TopCore.Auth/Controllers/Base/ApiController.cs
+++ TopCore.Auth/TopCore.Auth/Controllers/Base/ApiController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using TopCore.Auth.Filters;
 
 // ReSharper disable once CheckNamespace
 namespace TopCore.Auth.Controllers
 {
+    [ApiExceptionFilter]
     [Produces("application/json", "application/xml")]
     public class ApiController : Controller
     {

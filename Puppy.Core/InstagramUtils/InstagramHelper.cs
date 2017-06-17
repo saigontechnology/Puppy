9d03458a12c6db3b8a4d5f9bd6d82840a94f75c7
Top <topnguyen92@gmail.com>
2017-06-17 09:21:56 +0700

    InstagramHelper.cs

---
+++ Puppy.Core/InstagramUtils/InstagramHelper.cs
@@ -0,0 +1,55 @@
+﻿#region	License
+//------------------------------------------------------------------------------------------------
+// <License>
+//     <Copyright> 2017 © Top Nguyen → AspNetCore → Puppy </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Puppy </Project>
+//     <File>
+//         <Name> InstagramHelper.cs </Name>
+//         <Created> 17/06/2017 2:07:18 AM </Created>
+//         <Key> 3faa58e0-4566-4dd8-b606-06e6d194e9e0 </Key>
+//     </File>
+//     <Summary>
+//         InstagramHelper.cs
+//     </Summary>
+// <License>
+//------------------------------------------------------------------------------------------------
+#endregion License
+
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puppy.Core.InstagramUtils
+{
+    public static class InstagramHelper
+    {
+        /// <summary>
+        ///     Get Maximum 20 recent feeds of user 
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public static async Task<InstagramUserFeeds> GetUserFeeds(string accessToken, string userId = "self")
+        {
+            var endpoint = $"https://api.instagram.com/v1/users/{userId}/media/recent?access_token={accessToken}";
+
+            using (var client = new HttpClient())
+            {
+                // Request
+                var result = await client.GetAsync(endpoint);
+                var buffer = await result.Content.ReadAsByteArrayAsync();
+                var byteArray = buffer.ToArray();
+                var responseString = Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
+
+                InstagramUserFeeds instagramUserFeeds =
+                    JsonConvert.DeserializeObject<InstagramUserFeeds>(responseString);
+
+                return instagramUserFeeds;
+            }
+        }
+    }
+}
---
+++ Puppy.Core/InstagramUtils/InstagramUserFeeds.cs
@@ -0,0 +1,142 @@
+﻿#region	License
+//------------------------------------------------------------------------------------------------
+// <License>
+//     <Copyright> 2017 © Top Nguyen → AspNetCore → Puppy </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Puppy </Project>
+//     <File>
+//         <Name> InstagramUserFeeds.cs </Name>
+//         <Created> 17/06/2017 2:06:32 AM </Created>
+//         <Key> d415518a-1534-4c0d-9e03-eb54e17a30b8 </Key>
+//     </File>
+//     <Summary>
+//         InstagramUserFeeds.cs
+//     </Summary>
+// <License>
+//------------------------------------------------------------------------------------------------
+#endregion License
+
+using Newtonsoft.Json;
+
+namespace Puppy.Core.InstagramUtils
+{
+    public class InstagramUserFeeds
+    {
+        public Pagination pagination { get; set; }
+
+        public Datum[] data { get; set; }
+
+        public Meta meta { get; set; }
+    }
+
+    public class Pagination
+    {
+    }
+
+    public class Meta
+    {
+        public int code { get; set; }
+    }
+
+    public class Datum
+    {
+        public string id { get; set; }
+
+        public User user { get; set; }
+
+        public Images images { get; set; }
+
+        public string created_time { get; set; }
+
+        public object caption { get; set; }
+
+        public bool user_has_liked { get; set; }
+
+        public Likes likes { get; set; }
+
+        public object[] tags { get; set; }
+
+        public string filter { get; set; }
+
+        public Comments comments { get; set; }
+
+        public string type { get; set; }
+
+        public string link { get; set; }
+
+        public object location { get; set; }
+
+        public object attribution { get; set; }
+
+        public object[] users_in_photo { get; set; }
+
+        public Carousel_Media[] carousel_media { get; set; }
+    }
+
+    public class User
+    {
+        public string id { get; set; }
+
+        public string full_name { get; set; }
+
+        public string profile_picture { get; set; }
+
+        public string username { get; set; }
+    }
+
+    public class Images
+    {
+        public Thumbnail thumbnail { get; set; }
+
+        public Low_Resolution low_resolution { get; set; }
+
+        public Standard_Resolution standard_resolution { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        public int width { get; set; }
+
+        public int height { get; set; }
+
+        public string url { get; set; }
+    }
+
+    public class Low_Resolution
+    {
+        public int width { get; set; }
+
+        public int height { get; set; }
+
+        public string url { get; set; }
+    }
+
+    public class Standard_Resolution
+    {
+        public int width { get; set; }
+
+        public int height { get; set; }
+
+        public string url { get; set; }
+    }
+
+    public class Likes
+    {
+        public int count { get; set; }
+    }
+
+    public class Comments
+    {
+        public int count { get; set; }
+    }
+
+    public class Carousel_Media
+    {
+        public Images images { get; set; }
+
+        public object[] users_in_photo { get; set; }
+
+        public string type { get; set; }
+    }
+}

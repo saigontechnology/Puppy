03c1e868142a9fe4a14a644a351fd247098c39b0
Top <topnguyen92@gmail.com>
2017-08-24 13:58:05 +0700

    Add AutoMapper Extensions for object and queryable

---
+++ Puppy.AutoMapper/ObjectExtensions.cs
@@ -0,0 +1,52 @@
+﻿#region	License
+//------------------------------------------------------------------------------------------------
+// <License>
+//     <Copyright> 2017 © Top Nguyen → AspNetCore → Puppy </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Puppy </Project>
+//     <File>
+//         <Name> ObjectExtensions.cs </Name>
+//         <Created> 24/08/17 1:26:59 PM </Created>
+//         <Key> ea4c74e4-ea68-44b1-8908-b2c4db4ce477 </Key>
+//     </File>
+//     <Summary>
+//         ObjectExtensions.cs
+//     </Summary>
+// <License>
+//------------------------------------------------------------------------------------------------
+#endregion License
+
+using AutoMapper;
+
+namespace Puppy.AutoMapper
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        ///     Converts an object to another using AutoMapper library. Creates a new object of
+        ///     <typeparamref name="TDestination" />. There must be a mapping between objects before
+        ///     calling this method.
+        /// </summary>
+        /// <typeparam name="TDestination"> Type of the destination object </typeparam>
+        /// <param name="source"> Source object </param>
+        public static TDestination To<TDestination>(this object source)
+        {
+            return Mapper.Map<TDestination>(source);
+        }
+
+        /// <summary>
+        ///     Execute a mapping from the source object to the existing destination object There
+        ///     must be a mapping between objects before calling this method.
+        /// </summary>
+        /// <typeparam name="TSource"> Source type </typeparam>
+        /// <typeparam name="TDestination"> Destination type </typeparam>
+        /// <param name="source">      Source object </param>
+        /// <param name="destination"> Destination object </param>
+        /// <returns></returns>
+        public static TDestination To<TSource, TDestination>(this TSource source, TDestination destination)
+        {
+            return Mapper.Map(source, destination);
+        }
+    }
+}
---
+++ Puppy.AutoMapper/QueryableExtensions.cs
@@ -0,0 +1,61 @@
+﻿#region	License
+//------------------------------------------------------------------------------------------------
+// <License>
+//     <Copyright> 2017 © Top Nguyen → AspNetCore → Puppy </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Puppy </Project>
+//     <File>
+//         <Name> QueryableExtensions.cs </Name>
+//         <Created> 24/08/17 1:33:57 PM </Created>
+//         <Key> 113a8bf0-f62a-4fc1-a90a-40452ab4de79 </Key>
+//     </File>
+//     <Summary>
+//         QueryableExtensions.cs
+//     </Summary>
+// <License>
+//------------------------------------------------------------------------------------------------
+#endregion License
+
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Puppy.AutoMapper
+{
+    public static class QueryableExtensions
+    {
+        /// <summary>
+        ///     Extension method to project from a queryable using the provided mapping engine 
+        /// </summary>
+        /// <remarks> Projections are only calculated once and cached </remarks>
+        /// <typeparam name="TDestination"> Destination type </typeparam>
+        /// <param name="source">          Queryable source </param>
+        /// <param name="membersToExpand"> Explicit members to expand </param>
+        /// <returns> Expression to project into </returns>
+        public static IQueryable<TDestination> To<TDestination>(this IQueryable source, params Expression<Func<TDestination, object>>[] membersToExpand)
+        {
+            return source.ProjectTo(Mapper.Configuration, null, membersToExpand);
+        }
+
+        /// <summary>
+        ///     Projects the source type to the destination type given the mapping configuration 
+        /// </summary>
+        /// <typeparam name="TDestination"> Destination type to map to </typeparam>
+        /// <param name="source">          Queryable source </param>
+        /// <param name="parameters">     
+        ///     Optional parameter object for parameterized mapping expressions
+        /// </param>
+        /// <param name="membersToExpand"> Explicit members to expand </param>
+        /// <returns>
+        ///     Queryable result, use queryable extension methods to project and execute result
+        /// </returns>
+        public static IQueryable<TDestination> To<TDestination>(this IQueryable source, IDictionary<string, object> parameters, params string[] membersToExpand)
+        {
+            return source.ProjectTo<TDestination>(Mapper.Configuration, parameters, membersToExpand);
+        }
+    }
+}

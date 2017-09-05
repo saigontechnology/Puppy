1f7a51e7902bcd655e70309a22f36224c280d6b2
Top <topnguyen92@gmail.com>
2017-09-05 11:44:16 +0700

    add ObjectExtensions

---
+++ Puppy.Core/ObjectUtils/ObjectExtensions.cs
@@ -0,0 +1,98 @@
+﻿#region	License
+//------------------------------------------------------------------------------------------------
+// <License>
+//     <Copyright> 2017 © Top Nguyen → AspNetCore → Puppy </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Puppy </Project>
+//     <File>
+//         <Name> ObjectExtensions.cs </Name>
+//         <Created> 16/08/17 6:08:02 PM </Created>
+//         <Key> b207be99-bed7-45d7-8d0b-c26690d5444b </Key>
+//     </File>
+//     <Summary>
+//         ObjectExtensions.cs
+//     </Summary>
+// <License>
+//------------------------------------------------------------------------------------------------
+#endregion License
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Puppy.Core.ObjectUtils
+{
+    public static class ObjectExtensions
+    {
+        private const string ExpressionCannotBeNullMessage = "The expression cannot be null.";
+
+        private const string InvalidExpressionMessage = "Invalid expression.";
+
+        public static object GetPropertyValue<T>(this T instance, string propertyName)
+        {
+            var targetProperty = instance.GetType().GetProperty(propertyName);
+            return targetProperty?.GetValue(instance);
+        }
+
+        public static void SetProperty<T>(this T entityToSet, string propertyName, object value)
+        {
+            var targetProperty = entityToSet.GetType().GetProperty(propertyName);
+            targetProperty?.SetValue(entityToSet, value, null);
+        }
+
+        public static string GetMemberName<T>(this T instance, Expression<Func<T, object>> expression)
+        {
+            return GetMemberName(expression.Body);
+        }
+
+        public static List<string> GetMemberNames<T>(this T instance, params Expression<Func<T, object>>[] expressions)
+        {
+            List<string> memberNames = new List<string>();
+            foreach (var cExpression in expressions)
+            {
+                memberNames.Add(GetMemberName(cExpression.Body));
+            }
+            return memberNames;
+        }
+
+        public static string GetMemberName<T>(this T instance, Expression<Action<T>> expression)
+        {
+            return GetMemberName(expression.Body);
+        }
+
+        private static string GetMemberName(Expression expression)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentException(ExpressionCannotBeNullMessage);
+            }
+            if (expression is MemberExpression memberExpression)
+            {
+                // Reference type property or field
+                return memberExpression.Member.Name;
+            }
+            if (expression is MethodCallExpression methodCallExpression)
+            {
+                // Reference type method
+                return methodCallExpression.Method.Name;
+            }
+            if (expression is UnaryExpression unaryExpression)
+            {
+                // Property, field of method returning value type
+                return GetMemberName(unaryExpression);
+            }
+            throw new ArgumentException(InvalidExpressionMessage);
+        }
+
+        private static string GetMemberName(UnaryExpression unaryExpression)
+        {
+            if (unaryExpression.Operand is MethodCallExpression methodExpression)
+            {
+                return methodExpression.Method.Name;
+            }
+            return ((MemberExpression)unaryExpression.Operand).Member.Name;
+        }
+    }
+}

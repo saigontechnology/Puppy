09df4340fb6c1e33fe80f88fd6fd64572dad95f2
Top <topnguyen92@gmail.com>
2017-10-19 17:17:08 +0700

    add try-catch and check content is empty

--- Puppy.DocxToHtml/Converter.cs
+++ Puppy.DocxToHtml/Converter.cs
@@ -35,101 +35,120 @@ namespace Puppy.DocxToHtml
             var htmlFileInfo = new FileInfo(htmlPath);
             htmlFileInfo.Directory.Create();
 
-            using (MemoryStream memoryStream = new MemoryStream())
+            if (docxBytes.Length <= 0)
             {
-                memoryStream.Write(docxBytes, 0, docxBytes.Length);
-                using (WordprocessingDocument wDoc = WordprocessingDocument.Open(memoryStream, true))
+                // Create file with empty content
+                if (!File.Exists(htmlFileInfo.FullName))
                 {
-                    var imageDirectoryName = Path.Combine(htmlFileInfo.DirectoryName, $"{Path.GetFileNameWithoutExtension(htmlFileInfo.FullName)}_files");
+                    File.Create(htmlFileInfo.FullName);
+                }
 
-                    int imageCounter = 0;
+                return htmlFileInfo;
+            }
 
-                    var pageTitle = Path.GetFileNameWithoutExtension(docxFileInfo.Name);
-
-                    // TODO: Determine max-width from size of content area.
-                    HtmlConverterSettings settings = new HtmlConverterSettings
+            try
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    memoryStream.Write(docxBytes, 0, docxBytes.Length);
+                    using (WordprocessingDocument wDoc = WordprocessingDocument.Open(memoryStream, true))
                     {
-                        AdditionalCss = "body { margin: 1cm auto; max-width: 20cm; padding: 0; }",
-                        PageTitle = pageTitle,
-                        FabricateCssClasses = true,
-                        CssClassPrefix = "pt-",
-                        RestrictToSupportedLanguages = false,
-                        RestrictToSupportedNumberingFormats = false,
-                        ImageHandler = imageInfo =>
+                        var imageDirectoryName = Path.Combine(htmlFileInfo.DirectoryName, $"{Path.GetFileNameWithoutExtension(htmlFileInfo.FullName)}_files");
+
+                        int imageCounter = 0;
+
+                        var pageTitle = Path.GetFileNameWithoutExtension(docxFileInfo.Name);
+
+                        // TODO: Determine max-width from size of content area.
+                        HtmlConverterSettings settings = new HtmlConverterSettings
                         {
-                            ++imageCounter;
-
-                            string extension = imageInfo.ContentType.Split('/')[1].ToLower();
-
-                            ImageFormat imageFormat = null;
-
-                            if (extension == "png")
-                                imageFormat = ImageFormat.Png;
-                            else if (extension == "gif")
-                                imageFormat = ImageFormat.Gif;
-                            else if (extension == "bmp")
-                                imageFormat = ImageFormat.Bmp;
-                            else if (extension == "jpeg")
-                                imageFormat = ImageFormat.Jpeg;
-                            else if (extension == "tiff")
+                            AdditionalCss = "body { margin: 1cm auto; max-width: 20cm; padding: 0; }",
+                            PageTitle = pageTitle,
+                            FabricateCssClasses = true,
+                            CssClassPrefix = "pt-",
+                            RestrictToSupportedLanguages = false,
+                            RestrictToSupportedNumberingFormats = false,
+                            ImageHandler = imageInfo =>
                             {
-                                // Convert tiff to gif.
-                                extension = "gif";
-                                imageFormat = ImageFormat.Gif;
+                                ++imageCounter;
+
+                                string extension = imageInfo.ContentType.Split('/')[1].ToLower();
+
+                                ImageFormat imageFormat = null;
+
+                                if (extension == "png")
+                                    imageFormat = ImageFormat.Png;
+                                else if (extension == "gif")
+                                    imageFormat = ImageFormat.Gif;
+                                else if (extension == "bmp")
+                                    imageFormat = ImageFormat.Bmp;
+                                else if (extension == "jpeg")
+                                    imageFormat = ImageFormat.Jpeg;
+                                else if (extension == "tiff")
+                                {
+                                    // Convert tiff to gif.
+                                    extension = "gif";
+                                    imageFormat = ImageFormat.Gif;
+                                }
+                                else if (extension == "x-wmf")
+                                {
+                                    extension = "wmf";
+                                    imageFormat = ImageFormat.Wmf;
+                                }
+
+                                // If the image format isn't one that we expect, ignore it, and don't
+                                // return markup for the link.
+                                if (imageFormat == null)
+                                    return null;
+
+                                string imageFileName = $"{imageDirectoryName}/image_{imageCounter.ToString()}.{extension}";
+
+                                try
+                                {
+                                    imageInfo.Bitmap.Save(imageFileName, imageFormat);
+                                }
+                                catch (System.Runtime.InteropServices.ExternalException)
+                                {
+                                    return null;
+                                }
+
+                                string imageSource = $"{htmlFileInfo.Directory.Name}/image_{imageCounter.ToString()}.{extension}";
+
+                                XElement img =
+                                    new XElement(
+                                        Xhtml.img,
+                                        new XAttribute(NoNamespace.src, imageSource),
+                                        imageInfo.ImgStyleAttribute,
+                                        imageInfo.AltText != null
+                                            ? new XAttribute(NoNamespace.alt, imageInfo.AltText)
+                                            : null);
+                                return img;
                             }
-                            else if (extension == "x-wmf")
-                            {
-                                extension = "wmf";
-                                imageFormat = ImageFormat.Wmf;
-                            }
+                        };
 
-                            // If the image format isn't one that we expect, ignore it, and don't
-                            // return markup for the link.
-                            if (imageFormat == null)
-                                return null;
+                        XElement htmlElement = HtmlConverter.ConvertToHtml(wDoc, settings);
 
-                            string imageFileName = $"{imageDirectoryName}/image_{imageCounter.ToString()}.{extension}";
+                        // Produce HTML document with <!DOCTYPE html > declaration to tell the
+                        // browser we are using HTML5.
+                        var html = new XDocument(new XDocumentType("html", null, null, null), htmlElement);
 
-                            try
-                            {
-                                imageInfo.Bitmap.Save(imageFileName, imageFormat);
-                            }
-                            catch (System.Runtime.InteropServices.ExternalException)
-                            {
-                                return null;
-                            }
+                        // Note: the xhtml returned by ConvertToHtmlTransform contains objects of type
+                        // XEntity. PtOpenXmlUtil.cs define the XEntity class. See
+                        // http://blogs.msdn.com/ericwhite/archive/2010/01/21/writing-entity-references-using-linq-to-xml.aspx
+                        // for detailed explanation. // If you further transform the XML tree
+                        // returned by ConvertToHtmlTransform, you must do it correctly, or entities
+                        // will not be serialized properly.
 
-                            string imageSource = $"{htmlFileInfo.Directory.Name}/image_{imageCounter.ToString()}.{extension}";
-
-                            XElement img =
-                                new XElement(
-                                    Xhtml.img,
-                                    new XAttribute(NoNamespace.src, imageSource),
-                                    imageInfo.ImgStyleAttribute,
-                                    imageInfo.AltText != null
-                                        ? new XAttribute(NoNamespace.alt, imageInfo.AltText)
-                                        : null);
-                            return img;
-                        }
-                    };
-
-                    XElement htmlElement = HtmlConverter.ConvertToHtml(wDoc, settings);
-
-                    // Produce HTML document with <!DOCTYPE html > declaration to tell the browser we
-                    // are using HTML5.
-                    var html = new XDocument(new XDocumentType("html", null, null, null), htmlElement);
-
-                    // Note: the xhtml returned by ConvertToHtmlTransform contains objects of type
-                    // XEntity. PtOpenXmlUtil.cs define the XEntity class. See
-                    // http://blogs.msdn.com/ericwhite/archive/2010/01/21/writing-entity-references-using-linq-to-xml.aspx
-                    // for detailed explanation. // If you further transform the XML tree returned by
-                    // ConvertToHtmlTransform, you must do it correctly, or entities will not be
-                    // serialized properly.
-
-                    var htmlString = html.ToString(SaveOptions.DisableFormatting);
-                    File.WriteAllText(htmlFileInfo.FullName, htmlString, Encoding.UTF8);
+                        var htmlString = html.ToString(SaveOptions.DisableFormatting);
+                        File.WriteAllText(htmlFileInfo.FullName, htmlString, Encoding.UTF8);
+                    }
                 }
             }
+            catch
+            {
+                // Ignore
+            }
+
             return htmlFileInfo;
         }
     }

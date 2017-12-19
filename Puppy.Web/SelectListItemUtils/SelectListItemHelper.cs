8388aa86a29eb2b3fb9e9e2df8c3e2fbb470f35e
Top Nguyen <topnguyen92@gmail.com>
2017-12-19 13:13:51 +0700

    update namespace

--- Puppy.Web/SelectListItemUtils/SelectListItemHelper.cs
+++ Puppy.Web/SelectListItemUtils/SelectListItemHelper.cs
@@ -23,7 +23,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Puppy.Web
+namespace Puppy.Web.SelectListItemUtils
 {
     public static class SelectListItemHelper
     {

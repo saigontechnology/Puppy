2ae386907bcf2c74d95152559419dbfc0d6ce0d0
Top Nguyen <topnguyen92@gmail.com>
2018-01-18 16:11:24 +0700

    Support Cloudfare

--- Puppy.Web/HttpUtils/HttpDetection/HttpDetectionHelper.cs
+++ Puppy.Web/HttpUtils/HttpDetection/HttpDetectionHelper.cs
@@ -205,6 +205,22 @@ namespace Puppy.Web.HttpUtils.HttpDetection
         {
             var ipAddress = string.Empty;
 
+            // Priority to Proxy Server
+
+            if (request.Headers.TryGetValue(HeaderKey.CFConnectingIP, out var cloudFareConnectingIp))
+            {
+                ipAddress = cloudFareConnectingIp;
+
+                return ipAddress;
+            }
+
+            if (request.Headers.TryGetValue(HeaderKey.CFTrueClientIP, out var cloudFareTrueClientIp))
+            {
+                ipAddress = cloudFareTrueClientIp;
+
+                return ipAddress;
+            }
+
             // Look for the X-Forwarded-For (XFF) HTTP header field it's used for identifying the
             // originating IP address of a client connecting to a web server through an HTTP proxy or
             // load balancer.

e87362201befefa11b1b4b6714e347f794027bb6
Top Nguyen <topnguyen92@gmail.com>
2018-01-24 15:42:48 +0700

    update DateTimeExtensions

--- Puppy.Core/DateTimeUtils/DateTimeExtensions.cs
+++ Puppy.Core/DateTimeUtils/DateTimeExtensions.cs
@@ -72,36 +72,70 @@ namespace Puppy.Core.DateTimeUtils
         /// <summary>
         ///     Truncate date time 
         /// </summary>
-        /// <param name="dt">        </param>
-        /// <param name="truncateTo"></param>
+        /// <param name="dt">         </param>
+        /// <param name="withoutType"></param>
         /// <returns></returns>
-        public static DateTime TruncateTo(this DateTime dt, Enums.DateTruncate truncateTo)
+        public static DateTime Without(this DateTime dt, Enums.DateTimeWithoutType withoutType)
         {
-            if (truncateTo == Enums.DateTruncate.Year)
-                return new DateTime(dt.Year, 0, 0);
-
-            if (truncateTo == Enums.DateTruncate.Month)
-                return new DateTime(dt.Year, dt.Month, 0);
-
-            if (truncateTo == Enums.DateTruncate.Day)
-                return new DateTime(dt.Year, dt.Month, dt.Day);
-
-            if (truncateTo == Enums.DateTruncate.Hour)
-                return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, 0, 0);
-
-            if (truncateTo == Enums.DateTruncate.Minute)
-                return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
-
-            return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, 0);
+            switch (withoutType)
+            {
+                case Enums.DateTimeWithoutType.Month:
+                    {
+                        return new DateTime(dt.Year, 0, 0);
+                    }
+                case Enums.DateTimeWithoutType.Day:
+                    {
+                        return new DateTime(dt.Year, dt.Month, 0);
+                    }
+                case Enums.DateTimeWithoutType.Hour:
+                    {
+                        return new DateTime(dt.Year, dt.Month, dt.Day);
+                    }
+                case Enums.DateTimeWithoutType.Minute:
+                    {
+                        return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, 0, 0);
+                    }
+                case Enums.DateTimeWithoutType.Second:
+                    {
+                        return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
+                    }
+                default:
+                    {
+                        return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, 0);
+                    }
+            }
         }
 
         /// <summary>
-        ///     Get Date Time without milliseconds 
+        ///     Truncate date time 
         /// </summary>
+        /// <param name="dt">         </param>
+        /// <param name="withoutType"></param>
         /// <returns></returns>
-        public static DateTime WithoutMillisecond(DateTime dateTime)
+        public static DateTimeOffset Without(this DateTimeOffset dt, Enums.DateTimeWithoutType withoutType)
         {
-            return TruncateTo(dateTime, Enums.DateTruncate.Second);
+            switch (withoutType)
+            {
+                case Enums.DateTimeWithoutType.Month:
+                    return new DateTimeOffset(dt.Year, 0, 0, 0, 0, 0, dt.Offset);
+
+                case Enums.DateTimeWithoutType.Day:
+                    return new DateTimeOffset(dt.Year, dt.Month, 0, 0, 0, 0, dt.Offset);
+
+                case Enums.DateTimeWithoutType.Hour:
+                    return new DateTimeOffset(dt.Year, dt.Month, dt.Day, 0, 0, 0, dt.Offset);
+
+                case Enums.DateTimeWithoutType.Minute:
+                    return new DateTimeOffset(dt.Year, dt.Month, dt.Day, dt.Hour, 0, 0, dt.Offset);
+
+                case Enums.DateTimeWithoutType.Second:
+                    return new DateTimeOffset(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0, dt.Offset);
+
+                default:
+                    {
+                        return new DateTimeOffset(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, 0, dt.Offset);
+                    }
+            }
         }
 
         /// <summary>
@@ -127,14 +161,14 @@ namespace Puppy.Core.DateTimeUtils
 
     public class Enums
     {
-        public enum DateTruncate
+        public enum DateTimeWithoutType
         {
-            Year,
             Month,
             Day,
             Hour,
             Minute,
-            Second
+            Second,
+            Milliseconds
         }
     }
 }

7bd2cb3e619b3b7ed9ae27370992def6e5a0c2e5
Phuong Tran <phuong.tran@saigontechnology.com>
2019-04-23 13:16:14 +0700

    SFTP helper/ Support multiple files and ssh key

--- Puppy.Core/SFTPUltils/SFTPUtils.cs
+++ Puppy.Core/SFTPUltils/SFTPUtils.cs
@@ -1,24 +1,4 @@
-﻿#region	License
-
-//------------------------------------------------------------------------------------------------
-// <License>
-//     <Copyright> 2017 © Top Nguyen → AspNetCore → TopCore </Copyright>
-//     <Url> http://topnguyen.net/ </Url>
-//     <Author> Top </Author>
-//     <Project> TopCore </Project>
-//     <File>
-//         <Name> StringHelper.cs </Name>
-//         <Created> 24 Apr 17 10:44:55 PM </Created>
-//         <Key> dbf0d9b6-5968-47f0-b083-8375bb027161 </Key>
-//     </File>
-//     <Summary>
-//         StringHelper.cs
-//     </Summary>
-// <License>
-//------------------------------------------------------------------------------------------------
-
-#endregion License
-
+﻿using System.Collections.Generic;
 using System.IO;
 using Renci.SshNet;
 
@@ -29,24 +9,46 @@ namespace Puppy.Core.SFTPUltils
         /// <summary>
         ///     Upload file to sftp
         /// </summary>
-        /// <param name="host"></param>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
-        /// <param name="sourcePath"></param>
-        /// <param name="destinationPath"></param>
-        /// <param name="port"></param>
-        public static void UploadFile(string host, string username, string password, string sourcePath, string destinationPath, int port)
+        /// <param name="host">IP of SFTP</param>
+        /// <param name="privateKeyPath">Private key path. Must be .ppk</param>
+        /// <param name="username">Username to authenticate</param>
+        /// <param name="password">Password to authenticate</param>
+        /// <param name="sourcePaths">Files to upload</param>
+        /// <param name="destinationPath">Destination folder</param>
+        /// <param name="port">Port</param>
+        /// <param name="bufferSize">Buffer size</param>
+        public static void UploadFiles(string host, string privateKeyPath, string username, string password, string[] sourcePaths, string destinationPath, int port = 22, uint bufferSize = 4*2014)
         {
-            using (var client = new SftpClient(host, port, username, password))
+            var methods = new List<AuthenticationMethod> {new PasswordAuthenticationMethod(username, password)};
+
+            // Add private key
+            if (!string.IsNullOrEmpty(privateKeyPath))
+            {
+                var keyFile = new PrivateKeyFile(privateKeyPath);
+                var keyFiles = new[] { keyFile };
+
+                methods.Add(new PrivateKeyAuthenticationMethod(username, keyFiles));
+            }
+
+            var con = new ConnectionInfo(host, port, username, methods.ToArray());
+
+            using (var client = new SftpClient(con))
             {
                 client.Connect();
                 client.ChangeDirectory(destinationPath);
 
-                using (var fs = new FileStream(sourcePath, FileMode.Open))
+                foreach (var sourcePath in sourcePaths)
                 {
-                    client.BufferSize = 4 * 1024;
-                    client.UploadFile(fs, Path.GetFileName(sourcePath));
+                    if(!File.Exists(sourcePath)) continue;
+
+                    using (var fs = new FileStream(sourcePath, FileMode.Open))
+                    {
+                        client.BufferSize = bufferSize;
+                        client.UploadFile(fs, Path.GetFileName(sourcePath), true);
+                    }
                 }
+
+                client.Disconnect();
             }
         }
     }

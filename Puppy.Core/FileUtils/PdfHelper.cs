878a885b3afeb0740ef0bba9a491f94134b8d5f8
Linh Nguyen <linh.nguyen@saigontechnology.com>
2019-05-14 18:10:50 +0700

    add merge pdf file

--- Puppy.Core/FileUtils/PdfHelper.cs
+++ Puppy.Core/FileUtils/PdfHelper.cs
@@ -17,9 +17,12 @@
 //------------------------------------------------------------------------------------------------
 #endregion License
 
+using System;
+using System.Collections.Generic;
 using iTextSharp.text.pdf;
 using Puppy.Core.StringUtils;
 using System.IO;
+using iTextSharp.text;
 
 namespace Puppy.Core.FileUtils
 {
@@ -60,6 +63,58 @@ namespace Puppy.Core.FileUtils
                     PdfReader reader = new PdfReader(input);
                     PdfEncryptor.Encrypt(reader, output, true, password, password, PdfWriter.ALLOW_SCREENREADERS);
                 }
+            }
+        }
+
+        /// <summary>
+        /// Merge pdf files.
+        /// </summary>
+        /// <param name="sourceFiles">PDF files being merged.</param>
+        /// <returns></returns>
+        public static byte[] MergeFiles(List<byte[]> sourceFiles)
+        {
+            Document document = new Document();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                PdfCopy copy = new PdfCopy(document, ms);
+                document.Open();
+                int documentPageCounter = 0;
+
+                // Iterate through all pdf documents
+                for (int fileCounter = 0; fileCounter < sourceFiles.Count; fileCounter++)
+                {
+                    // Create pdf reader
+                    PdfReader reader = new PdfReader(sourceFiles[fileCounter]);
+                    int numberOfPages = reader.NumberOfPages;
+
+                    // Iterate through all pages
+                    for (int currentPageIndex = 1; currentPageIndex <= numberOfPages; currentPageIndex++)
+                    {
+                        documentPageCounter++;
+                        PdfImportedPage importedPage = copy.GetImportedPage(reader, currentPageIndex);
+                        PdfCopy.PageStamp pageStamp = copy.CreatePageStamp(importedPage);
+
+                        //// Write header
+                        //ColumnText.ShowTextAligned(pageStamp.GetOverContent(), Element.ALIGN_CENTER,
+                        //    new Phrase("PDF Merger "), importedPage.Width / 2, importedPage.Height - 30,
+                        //    importedPage.Width < importedPage.Height ? 0 : 1);
+
+                        // Write footer
+                        ColumnText.ShowTextAligned(pageStamp.GetOverContent(), Element.ALIGN_CENTER,
+                            new Phrase(String.Format("Page {0}", documentPageCounter)), importedPage.Width / 2, 30,
+                            importedPage.Width < importedPage.Height ? 0 : 1);
+
+                        pageStamp.AlterContents();
+
+                        copy.AddPage(importedPage);
+                    }
+
+                    copy.FreeReader(reader);
+                    reader.Close();
+                }
+
+                document.Close();
+                return ms.GetBuffer();
             }
         }
     }
